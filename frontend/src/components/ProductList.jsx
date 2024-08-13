685ae6b6b6cb5ec8c30cc35aa5a29fa6de909b82
Anugrah P <anugrahprathap@gmail.com>
2024-08-13 14:40:24 +0530

    update map error

--- frontend/src/components/ProductList.jsx
+++ frontend/src/components/ProductList.jsx
@@ -137,7 +137,7 @@ function ProductList({ category }) {
         <div>Loading...</div>
       ) : (
         <Slider {...settings}>
-          {products.map((product,index) => (
+          {products && products.map((product,index) => (
             <div key={product.ProductId} className="">
               <Link
                 to={`/product/${product.ProductId}`}
